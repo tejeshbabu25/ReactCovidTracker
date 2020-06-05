698fcb3c6f8f1ab06234669fc9d2a5662caf62e9
Tejesh <tejeshbabu25@gmail.com>
2020-06-05 09:39:10 +0530

    Updates

---
+++ precache-manifest.8fc8cab157fafd09712eff722d366c6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f363446191731c46d0c5b41ac18b76a",
+    "url": "/ReactCovidTracker/index.html"
+  },
+  {
+    "revision": "85f9bd96a1175f985596",
+    "url": "/ReactCovidTracker/static/css/main.63506031.chunk.css"
+  },
+  {
+    "revision": "82a013fac6d6a5f6d0ed",
+    "url": "/ReactCovidTracker/static/js/2.9640c575.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/ReactCovidTracker/static/js/2.9640c575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f9bd96a1175f985596",
+    "url": "/ReactCovidTracker/static/js/main.db0efc41.chunk.js"
+  },
+  {
+    "revision": "2bae7509a734ce751221",
+    "url": "/ReactCovidTracker/static/js/runtime-main.ad13a91c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/ReactCovidTracker/static/media/image.d7265326.png"
+  }
+]);
